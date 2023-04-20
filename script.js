ad1a06fa88553b47db7671a9b575916837f2ffae
joan-kii <joan.sb@hotmail.com>
2023-04-20 05:38:56 +0200

    añade menu button

--- script.js
+++ script.js
@@ -1,25 +1,16 @@
-const canvas = document.getElementById('particles-container');
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+// Menu dropdown handler
+const menuButton = document.getElementById('menu');
+const closeButton = document.getElementById('close');
 
-const ctx = canvas.getContext('2d');
-const particles = [];
 
-for (let i = 0; i < 300; i++) {
-  const x = Math.floor(Math.random() * canvas.width);
-  const y = Math.floor(Math.random() * canvas.height);
-  const size = Math.floor(Math.random() * 10);
-  const opacity = Math.random().toFixed(1);
+// segui aquí (no funciona addEventListener)
+menuButton.addEventListener('click', () => {
+  console.log('lol')
+  menuButton.style.display = 'none';
+  closeButton.style.display = 'block';
+})
 
-  ctx.beginPath();
-  ctx.fillStyle = `rgba(255, 255, 255, ${opacity})`;
-  ctx.arc(x, y, size, 0, Math.PI * 2, false);
-  ctx.fill();
-}
-
-function moveParticles() {
-  const speed = 1;
-  requestAnimationFrame(moveParticles);
-}
-
-/* moveParticles(); */
+closeButton.addEventListener('click', () => {
+  closeButton.style.display = 'none';
+  menuButton.style.display = 'block';
+})
