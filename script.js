03d5633aef37ddf58a28e13729d116ba759ef8fa
joan-kii <joan.sb@hotmail.com>
2023-05-04 05:39:41 +0200

    añade emailjs

--- script.js
+++ script.js
@@ -20,3 +20,26 @@ closeButton.addEventListener('click', () => {
   menuButton.style.display = 'block';
   menuDropdown.style.display = 'none';
 })
+
+// Emailjs handler
+function emailjs() {
+  emailjs.init('6k3-Gl0ANeJNcqsaT');
+};
+
+window.onload = function() {
+  document.getElementById('contact-form').addEventListener('submit', function(event) {
+    console.log('submitted');
+    event.preventDefault();
+    // generate a five digit number for the contact_number variable
+    this.contact_number.value = Math.random() * 100000 | 0;
+    // these IDs from the previous steps
+    emailjs.sendForm('contact_service', 'contact_form', this)
+      .then(function() {
+        console.log('SUCCESS!');
+      }, function(error) {
+        console.log('FAILED...', error);
+      });
+  });
+}
+
+emailjs();
